1ee751e486728248083346e555e1c847ef744e7a
Isa Rivera <chelachabela@hotmail.com>
2016-12-04 11:40:57 -0600

    Initial Commit

---
+++ prueba 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba 1
+//
+//  Created by Graciela Rivera on 04/12/16.
+//  Copyright © 2016 Graciela Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
